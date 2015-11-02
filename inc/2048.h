a955d86832be19db7b5a7361154b15da10138d90
Jordan Zink <jordanzink@gmail.com>
2015-11-01 20:21:05 -0500

    basic state initialization and random policy

--- inc/2048.h
+++ inc/2048.h
@@ -5,8 +5,38 @@
 //#define NDEBUG    //uncomment this to turn off asserts
 #include <assert.h>
 
+#include <vector>
+
+using std::vector;
+
 static const int NUM_ROWS = 4;
 static const int NUM_COLS = 4;
+
+enum class Move_2048 {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};
+
+class Random_Policy {
+
+private:
+
+    vector<int> values;
+    vector<int> weights;
+    int total_weight;
+
+    int rand_int(int lo, int hi); //[lo, hi)
+
+public:
+
+    Random_Policy(int n, int* values_in, int* weights_in, int random_seed);
+    ~Random_Policy();
+
+    void get_random_position_and_value(int num_position, int &position_to_use, int &value);
+
+};
 
 class State_2048 {
 
@@ -14,11 +44,17 @@ private:
 
     int board[NUM_ROWS][NUM_COLS];
     int score;
+    Random_Policy* random_policy;
+
+    void reset_to_blank_state();
+    bool add_random_cell();
 
 public:
 
-    State_2048();
+    State_2048(Random_Policy* random_policy);
     ~State_2048();
+
+    void reset_to_initial_state();
 
     int get_cell_value(int row, int col);
 
@@ -32,7 +68,7 @@ private:
 
 public:
 
-    Game_2048();
+    Game_2048(Random_Policy* random_policy);
     ~Game_2048();
 
     void play_game();
--- src/2048.cpp
+++ src/2048.cpp
@@ -1,7 +1,52 @@
 
 #include "2048.h"
 
-State_2048::State_2048() {
+#include <stdlib.h> //rand, srand
+
+Random_Policy::Random_Policy(int n, int* values_in, int* weights_in, int random_seed) {
+    this->values.assign(values_in, values_in + n);
+    this->weights.assign(weights_in, weights_in + n);
+
+    this->total_weight = 0;
+    for (int weight : this->weights) {
+        this->total_weight += weight;
+    }
+
+    srand(random_seed);
+}
+
+Random_Policy::~Random_Policy() {
+    //nothing right now...
+}
+
+int Random_Policy::rand_int(int lo, int hi) {
+    return rand() % (hi - lo) + lo;
+}
+
+void Random_Policy::get_random_position_and_value(int num_position, int &position_to_use, int &value) {
+    position_to_use = this->rand_int(0, num_position);
+
+    int weight_to_use = this->rand_int(0, this->total_weight);
+    bool found_weight_flag = false;
+    for (vector<int>::size_type i = 0; i < this->weights.size(); i++) {
+        int weight = this->weights[i];
+        if (weight_to_use < weight) {
+            value = this->values[i];
+            found_weight_flag = true;
+            break;
+        }
+        weight_to_use -= weight;
+    }
+    assert (found_weight_flag);
+
+    return;
+}
+
+State_2048::State_2048(Random_Policy* random_policy) {
+    this->random_policy = random_policy;
+}
+
+void State_2048::reset_to_blank_state() {
     for (int row = 0; row < NUM_ROWS; row++) {
         for (int col = 0; col < NUM_COLS; col++) {
             this->board[row][col] = 0;
@@ -10,8 +55,51 @@ State_2048::State_2048() {
     this->score = 0;
 }
 
+bool State_2048::add_random_cell() {
+    int num_empty_cells = 0;
+    for (int row = 0; row < NUM_ROWS; row++) {
+        for (int col = 0; col < NUM_COLS; col++) {
+            if (this->board[row][col] == 0) {
+                num_empty_cells++;
+            }
+        }
+    }
+    if (num_empty_cells == 0) {
+        return false;
+    }
+    
+    int position_to_use, value;
+    this->random_policy->get_random_position_and_value(num_empty_cells, position_to_use, value);
+
+    bool added_new_cell_flag = false;
+    for (int row = 0; row < NUM_ROWS; row++) {
+        for (int col = 0; col < NUM_COLS; col++) {
+            if (this->board[row][col] == 0) {
+                if (position_to_use == 0) {
+                    this->board[row][col] = value;
+                    added_new_cell_flag = true;
+                    break;
+                } else {
+                    position_to_use--;
+                }
+            }
+        }
+        if (added_new_cell_flag) {break;}
+    }
+    assert (added_new_cell_flag);
+
+    return true;
+}
+
 State_2048::~State_2048() {
     //nothing right now...
+}
+
+void State_2048::reset_to_initial_state() {
+    this->reset_to_blank_state();
+    // TODO this is currently hard coded to initially spawn two random cells; make this a config
+    this->add_random_cell();
+    this->add_random_cell();
 }
 
 int State_2048::get_cell_value(int row, int col) {
@@ -19,8 +107,9 @@ int State_2048::get_cell_value(int row, int col) {
     return this->board[row][col];
 }
 
-Game_2048::Game_2048() {
-    //nothing right now...
+Game_2048::Game_2048(Random_Policy* random_policy)
+        : cur_state(random_policy) {
+    //nothing right now
 }
 
 Game_2048::~Game_2048() {
